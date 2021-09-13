a9bf57aed51bb6df07d48f8828cefb5efa50eb1f
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-13 21:30:43 +0000

    Updated output strings to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,16 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("💚️💚️💚️ Assertation Passed: " + actual + " === " + expected);
+    console.log(`💚️💚️💚️ Assertation Passed: ${actual} === ${expected}`);
   } else {
-    console.log("💔️💔️💔️ Assertation Failed: " + actual + " !== " + expected);
+    console.log(`💔️💔️💔️ Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 
-// // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual('test string identical', 'test string identical');
-// assertEqual('This is correct', 'This Is correct');
-// assertEqual(9 - 2, 3 + 4);
-// assertEqual(9 - 2 * 2, (9 - 2) * 2);
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('test string identical', 'test string identical');
+assertEqual('This is correct', 'This Is correct');
+assertEqual(9 - 2, 3 + 4);
+assertEqual(9 - 2 * 2, (9 - 2) * 2);
