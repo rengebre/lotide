9bffa7a1b6763f7b018d8a3949c52fb9748d487d
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-13 21:43:33 +0000

    updated head.js after linting

--- head.js
+++ head.js
@@ -6,9 +6,9 @@
 //   }
 // };
 
-const head = function (array) {
+const head = function(array) {
   return array[0];
-}
+};
 
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
