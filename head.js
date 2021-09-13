4125353413dd6a512af5f009c9d93d88555af16d
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-13 21:40:45 +0000

    Added a new function called head defined in head.js

---
+++ head.js
@@ -0,0 +1,15 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`💚️💚️💚️ Assertation Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`💔️💔️💔️ Assertation Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const head = function (array) {
+  return array[0];
+}
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// console.log(head("Hello"));
