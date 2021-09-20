99ca028ad8760667a4bdf55d1ac7d8967bd361b5
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-20 15:50:04 +0000

    Refactored head & assertEqual to import functions and moved the test code to the test directory

--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`💚️💚️💚️ Assertation Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`💔️💔️💔️ Assertation Failed: ${actual} !== ${expected}`);
-//   }
-// };
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// console.log(head("Hello"));
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('test string identical', 'test string identical');
+assertEqual('This is correct', 'This Is correct');
+assertEqual(9 - 2, 3 + 4);
+assertEqual(9 - 2 * 2, (9 - 2) * 2);
