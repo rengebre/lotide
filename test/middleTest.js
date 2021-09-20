e2ee96b2a5aeba3e5ffa2ad136304e5a8137095d
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-20 20:48:10 +0000

    Added mocha/chai to middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,24 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]+
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+describe('#middle', () => {
+  it('should return an empty array when passed in an array of 1 element', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('should return an empty array when passed in an array of 2 elements', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it('should return [2] when passed in [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('should return a 2 element array when passed in an array with even number of elements', () => {
+    assert.strictEqual(middle([1, 2, 3, 4]).length, 2);
+  });
+
+  it('should return [2, 3] when passed in [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
